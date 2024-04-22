968da2a5da58709ba9a814d73a43ae77cb422d7b
zyhsu <jenny010328@gmail.com>
2024-04-22 21:26:03 +0800

    test : fix music path

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -11,20 +11,19 @@ const playlist = [
   {
     name: '都是 weather 你',
     author: '就以斯、楊子平',
-    file: '../../public/music/都是weather你.mp3',
+    file: 'public/music/都是weather你.mp3',
     // hide: true,
   },
   {
     name: 'love and pain',
     author: '이수현',
-    file: '../../public/music/love and pain.mp3',
+    file: 'public/music/love and pain.mp3',
   },
 ];
 
 export default {
-  // extends: DefaultTheme,
-  // Layout: Layout,
-  ...DefaultTheme,
+  extends: DefaultTheme,
+  Layout: Layout,
   enhanceApp: (ctx) => {
     // https://github.com/ZhongxuYang/vitepress-plugin-music
     vitepressMusic(playlist);
